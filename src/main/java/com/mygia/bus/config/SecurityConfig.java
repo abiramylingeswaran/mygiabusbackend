0be04600b0d061d3c7442b04fdc76e07a388f8dc
abiramylingeswaran <abiramylingeswaran23@gmail.com>
2026-08-21 12:30:21 +0530

    fix 403 error

--- src/main/java/com/mygia/bus/config/SecurityConfig.java
+++ src/main/java/com/mygia/bus/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/v1/routes/**").permitAll()
                         .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/reservations/**").authenticated()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
